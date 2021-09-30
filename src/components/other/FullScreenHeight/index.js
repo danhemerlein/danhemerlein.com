ecce892198307eb9cc991e3cbeb2cdabede8699c
Dan Hemerlein <danhemerlein@gmail.com>
2021-09-30 12:19:47 -0400

    get started on adding full screen height to filtered code projects

--- src/components/other/FullScreenHeight/index.js
+++ src/components/other/FullScreenHeight/index.js
@@ -13,9 +13,16 @@ const Container = styled.div`
 
   ${({ justify }) => justify && `justify-content: ${justify};`}
   ${({ items }) => items && `align-items: ${items};`}
+  ${({ direction }) => direction && `flex-direction: ${direction};`}
 `;
 
-const FullScreenHeight = ({ children, unsetBreakpoint, justify, items }) => {
+const FullScreenHeight = ({
+  children,
+  unsetBreakpoint,
+  justify,
+  items,
+  direction,
+}) => {
   const PADDING = 32;
   const HEADER_HEIGHT = 22;
   const FOOTER_HEIGHT = 22;
@@ -42,7 +49,7 @@ const FullScreenHeight = ({ children, unsetBreakpoint, justify, items }) => {
 
   return (
     <div style={{ height: generateHeight(breakpoint, height, offset) }}>
-      <Container justify={justify} items={items}>
+      <Container justify={justify} items={items} direction={direction}>
         {children}
       </Container>
     </div>
@@ -53,6 +60,7 @@ FullScreenHeight.propTypes = {
   unsetBreakpoint: string,
   justify: string,
   items: string,
+  direction: string,
   children: componentPropType.isRequired,
 };
 
@@ -60,6 +68,7 @@ FullScreenHeight.defaultProps = {
   unsetBreakpoint: 'none',
   justify: 'center',
   items: 'center',
+  direction: 'row',
 };
 
 export default FullScreenHeight;
--- src/components/pages/Code/FilteredProjects/index.js
+++ src/components/pages/Code/FilteredProjects/index.js
@@ -1,10 +1,15 @@
+import FullScreenHeight from 'components/other/FullScreenHeight';
 import { arrayOf, string } from 'prop-types';
 import { codeProjectPropTypes } from 'propTypes';
 import styled from 'styled-components';
-import { P } from 'styles/elements';
+import { FlexContainer, P } from 'styles/elements';
 import { remHelper } from 'utils';
 import { ListLinkContainer } from '../containers';
 import RenderProjects from '../RenderProjects';
+
+const StyledFlexContainer = styled(FlexContainer)`
+  width: 100%;
+`;
 
 const PageParagraph = styled(P)`
   width: 100%;
@@ -13,18 +18,38 @@ const PageParagraph = styled(P)`
 
 const FilteredProjects = ({ data, filterBy }) => {
   if (filterBy === 'work-experience') {
-    return <RenderProjects projects={data} hasImage />;
+    return (
+      <FullScreenHeight
+        items="flex-start"
+        justify="flex-start"
+        direction="column"
+      >
+        <RenderProjects projects={data} hasImage />
+      </FullScreenHeight>
+    );
   }
 
   if (filterBy === 'freelance') {
-    return <RenderProjects projects={data} highlight />;
+    return (
+      <FullScreenHeight
+        items="flex-start"
+        justify="flex-start"
+        direction="column"
+      >
+        <RenderProjects projects={data} highlight />
+      </FullScreenHeight>
+    );
   }
 
   if (filterBy === 'portfolios') {
     const listLinks = data.filter((project) => project.fields.isListLink);
     const nonListLinks = data.filter((project) => !project.fields.isListLink);
     return (
-      <>
+      <FullScreenHeight
+        items="flex-start"
+        justify="flex-start"
+        direction="column"
+      >
         <RenderProjects projects={nonListLinks} hasImage />
         <PageParagraph>
           Below are a few more passion projects in various states of completion:
@@ -32,12 +57,20 @@ const FilteredProjects = ({ data, filterBy }) => {
         <ListLinkContainer direction="column" wrap="wrap" items="center">
           <RenderProjects projects={listLinks} listLink hasImage={false} />
         </ListLinkContainer>
-      </>
+      </FullScreenHeight>
     );
   }
 
   if (filterBy === 'passion-project') {
-    return <RenderProjects projects={data} hasImage={false} />;
+    return (
+      <FullScreenHeight
+        items="flex-start"
+        justify="flex-start"
+        direction="column"
+      >
+        <RenderProjects projects={data} hasImage={false} />
+      </FullScreenHeight>
+    );
   }
 };
 

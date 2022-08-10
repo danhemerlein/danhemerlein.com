28c6cf6c11f13394d4342d35059a9102faa5070a
Dan Hemerlein <dan@halfhelix.com>
2022-08-10 08:38:18 -0400

    updates, eod

--- config-overrides.js
+++ config-overrides.js
@@ -1,35 +1,37 @@
 const webpack = require('webpack');
 
-module.exports = function override(config) {
-  config.resolve.fallback = {
-    assert: require.resolve('assert'),
-    crypto: require.resolve('crypto-browserify'),
-    http: require.resolve('stream-http'),
-    https: require.resolve('https-browserify'),
-    stream: require.resolve('stream-browserify'),
-    os: require.resolve('os')
-  };
+module.exports = {
+  webpack(config) {
+    config.resolve.fallback = {
+      assert: require.resolve('assert'),
+      crypto: require.resolve('crypto-browserify'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      stream: require.resolve('stream-browserify'),
+      os: require.resolve('os')
+    };
 
-  config.plugins.push(
-    new webpack.ProvidePlugin({
-      process: 'process/browser',
-      Buffer: ['buffer', 'Buffer']
-    })
-  );
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+        Buffer: ['buffer', 'Buffer']
+      })
+    );
 
-  config.stats = {
-    errorDetails: true
-  };
+    config.stats = {
+      errorDetails: true
+    };
 
-  config.optimization.splitChunks = {
-    cacheGroups: {
-      reactVendor: {
-        test: /[\\/]node_modules[\\/](react|react-dom|react-router-dom)[\\/]/,
-        name: 'vendor-react',
-        chunks: 'all'
+    config.optimization.splitChunks = {
+      cacheGroups: {
+        reactVendor: {
+          test: /[\\/]node_modules[\\/](react|react-dom|react-router-dom)[\\/]/,
+          name: 'vendor-react',
+          chunks: 'all'
+        }
       }
-    }
-  };
+    };
 
-  return config;
+    return config;
+  }
 };
--- src/utils/lib.js
+++ src/utils/lib.js
@@ -1,25 +1,3 @@
-/**
- * Returns string with first letter capitalized and all other letters lowercase
- * @param {string} string
- * @return {string}
- */
-
-export const capitalizeFirstLetter = (string) => {
-  if (typeof string !== 'string') return undefined;
-
-  return string.charAt(0).toUpperCase() + string.toLowerCase().slice(1);
-};
-
-export const addOrder = (content, orderSchema) => {
-  return content.map((item) => {
-    return (item.fields.order = orderSchema[item.fields.title].order);
-  });
-};
-
-export const compare = (a, b) => {
-  return a.fields.order - b.fields.order;
-};
-
 export const blockScroll = (bool) => {
   const html = document.getElementsByTagName('html')[0];
 

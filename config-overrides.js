fd1a886d087d7cf330912739fcc36cb3d9dd00b1
Dan Hemerlein <dan@halfhelix.com>
2022-08-10 09:56:12 -0400

    about to remove unused dependencies

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
@@ -18,16 +19,40 @@ module.exports = {
       })
     );
 
+    config.entry = {
+      main: path.resolve(__dirname, 'src/index.js'),
+      CloseIcon: path.resolve(__dirname, 'src/components/base/icons/Close.js'),
+      NotFoundIcon: path.resolve(
+        __dirname,
+        'src/components/base/icons/NotFound.js'
+      )
+    };
+
     config.stats = {
       errorDetails: true
     };
 
     config.optimization.splitChunks = {
+      /*
+        Webpack has some clever defaults that aren’t so clever, like a maximum
+        of 3 files when splitting the output files, and a minimum file size of 30
+        KB (all smaller files would be joined together). So I have overridden these.
+      */
+      minSize: 0,
+      chunks: 'all',
       cacheGroups: {
-        reactVendor: {
-          test: /[\\/]node_modules[\\/](react|react-dom|react-router-dom)[\\/]/,
-          name: 'vendor-react',
-          chunks: 'all'
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace('@', '')}`;
+          }
         }
       }
     };

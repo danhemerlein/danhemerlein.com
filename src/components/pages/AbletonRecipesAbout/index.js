f0e596198e67b94eddf96a0e3d3d9dd6ecc32311
Dan Hemerlein <dan@halfhelix.com>
2022-10-16 18:28:47 -0400

    Add some about page content

--- src/components/pages/AbletonRecipesAbout/index.js
+++ src/components/pages/AbletonRecipesAbout/index.js
@@ -1,4 +1,7 @@
 import { useEffect, useMemo, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { TwitterTweetEmbed } from 'react-twitter-embed';
+
 import styled from 'styled-components';
 import { FlexContainer, H1, P } from 'styles/elements';
 import {
@@ -16,9 +19,14 @@ const Container = styled.div`
     font-family: 'arial' !important;
   }
 `;
+
 const Inner = styled(FlexContainer)`
   max-width: 640px;
   margin: 0 auto;
+
+  blockquote {
+    width: 75%;
+  }
 `;
 
 const Stats = styled(FlexContainer)`
@@ -79,7 +87,7 @@ const AbletonRecipesAbout = () => {
       <Container>
         <Header />
         <Inner justify="center" items="center" direction="column">
-          <H1 textAlign="center">It's a recipes website for ableton live</H1>
+          <H1 textAlign="center">it's a recipes website for ableton live</H1>
           {posts.length &&
           ops.length &&
           platforms.length &&
@@ -99,7 +107,6 @@ const AbletonRecipesAbout = () => {
           <Paragraph bold italic>
             what is ableton recipes?
           </Paragraph>
-
           <Paragraph>
             Ableton Recipes is an online index of ableton live resources. It
             contains links to social media posts, sample packs, plugins/devices,
@@ -108,23 +115,25 @@ const AbletonRecipesAbout = () => {
             searchable database. Use it to learn something new. Use it for
             inspiration while producing. Use it to find new content creators to
             follow. Create an account to save favorite posts for later use.
-            Ableton Recipes is a passion project created by Dan Hemerlein, coded
-            and maintained with love in Brooklyn, New York.
           </Paragraph>
-
+          <Paragraph>
+            Ableton Recipes is a passion project created by{' '}
+            <Link to="/">Dan Hemerlein</Link> — coded and maintained with love
+            in Brooklyn, New York.
+          </Paragraph>
           <Paragraph bold italic>
             why did i make this?
           </Paragraph>
-
           <Paragraph>
-            The ethos of ableton recipes is captured in the blow quote which I
-            originally encounctered as a&nbsp;
+            I built Ableton Recipes because it's the online resources/learning
+            tool for Live that I always wish existed. The ethos behind this tool
+            is captured well in the following post I found on&nbsp;
             <a
               href="https://www.are.na/block/16481902"
               target="_blank"
               rel="noreferrer"
             >
-              post on are.na
+              are.na
             </a>
             &nbsp;from&nbsp;
             <a
@@ -135,9 +144,49 @@ const AbletonRecipesAbout = () => {
               Alice Otineo:
             </a>
           </Paragraph>
+          <Paragraph as="blockquote">
+            "At this point, we’ve gotten really great at “writing it down” We’ve
+            been trained to take notes, snag photos, and add things to our
+            bookmarks so we don’t forget about them. The more important question
+            that nobody has bothered to ask is what to do with it all.
+            Components are ultimately the building blocks of a story, project,
+            or idea, but accumulation means nothing without connection. What are
+            the footprints of your life adding up to?"
+          </Paragraph>
+
+          <Paragraph>
+            When I started learning Ableton, I found myself constantly ‘writing
+            it down’ in the form of bookmarking tweets and saving instagram
+            posts to collections. But I would rarely return to that content when
+            I was actually sitting down to produce music. If I did return to
+            those posts, I would have to open instagram and click through a
+            bunch of UI just to find a 15 second post that <em>might</em> inform
+            what I was working on. This process was slow and had many
+            opportunities for me to bounce and lose focus on the task at hand:
+            making dope music.
+          </Paragraph>
+
+          <Paragraph>
+            Here's a great twitter thread of others 'writing it down' in public:
+          </Paragraph>
+
+          <TwitterTweetEmbed tweetId="1580207543087136768" />
+
+          <Paragraph>
+            Ableton Recipes aims to be the place that has the ableton tips. This
+            index attempts to de-couple and contextualize the content contained
+            herein so site visitors can start applying the learnings from this
+            content with ease and speed.
+          </Paragraph>
 
           <Paragraph bold italic>
             what's next?
+          </Paragraph>
+
+          <Paragraph>
+            For now, I'm building this in the open. I'm trying to add new
+            content all the time and share the site with trusted friends for
+            contructive feedback. Thanks for being here.
           </Paragraph>
         </Inner>
       </Container>

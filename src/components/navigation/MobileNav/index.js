456bb54638df21faa3357d70ecf9e5df9396f557
Dan Hemerlein <dan@halfhelix.com>
2023-07-24 21:30:02 -0400

    remove comments

--- src/components/navigation/MobileNav/index.js
+++ src/components/navigation/MobileNav/index.js
@@ -57,10 +57,8 @@ const MobileNav = ({
   }
 
   useEffect(() => {
-    // Attach event listener to the 'resize' event
     window.addEventListener('resize', handleResize)
 
-    // Clean up the event listener when the component unmounts
     return () => {
       window.removeEventListener('resize', handleResize)
     }
